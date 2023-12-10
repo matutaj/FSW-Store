41bf2a939ba7d13ae02822bb4ee3ea95b6973d54
matutaj <matutajorge2002@gmail.com>
2023-12-10 22:56:06 +0100

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className=" bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+      @2023 FSW <span className="font-semibold"> By: Matuta Jorge</span>
+    </div>
+  );
+};
+
+export default Footer;
