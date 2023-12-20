469dd9d95b0cae8a2282a63f7878cd4004b13ea9
matutaj <matutajorge2002@gmail.com>
2023-12-20 01:37:01 +0100

    feat: add category

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import ProductItem from "@/components/productItem";
 import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/category-icon";
 import { ComputeProductTotalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
 import { ShapesIcon } from "lucide-react";
@@ -20,8 +21,8 @@ const CategoryProducts = async ({ params }: any) => {
           className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
           variant="outline"
         >
-          <ShapesIcon size={16} />
-          Catálogo
+          {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
+          {params.slug}
         </Badge>
 
         <div className="grid grid-cols-2  gap-8">
