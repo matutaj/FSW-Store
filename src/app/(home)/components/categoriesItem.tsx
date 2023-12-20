a979093793c750742399f63c8b3cab2071ad649b
matutaj <matutajorge2002@gmail.com>
2023-12-20 01:56:24 +0100

    feat: add link category

--- src/app/(home)/components/categoriesItem.tsx
+++ src/app/(home)/components/categoriesItem.tsx
@@ -1,19 +1,22 @@
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 interface CategoriesItemProps {
   category: Category;
 }
 
 const CategoriesItem = ({ category }: CategoriesItemProps) => {
   return (
-    <Badge
-      variant="outline"
-      className="flex items-center justify-center gap-2 py-3"
-    >
-      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-      <span className=" text-xs font-bold"> {category.name}</span>
-    </Badge>
+    <Link href={`category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex items-center justify-center gap-2 py-3"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className=" text-xs font-bold"> {category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
