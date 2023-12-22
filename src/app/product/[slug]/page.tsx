f7b4cb9ac9e2ad0b8d305f6546ade8c0dd3c3935
matutaj <matutajorge2002@gmail.com>
2023-12-22 14:37:31 +0100

    feat: add product info component

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/productI-mages";
+import ProductoInfo from "./components/product-inf";
+import { ComputeProductTotalPrice } from "@/helpers/product";
 
 interface ProductDetailsPageProps {
   params: {
@@ -18,8 +20,9 @@ const ProductDetailsPage = async ({
 
   if (!product) return null;
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <ProductoInfo product={ComputeProductTotalPrice(product)} />
     </div>
   );
 };
