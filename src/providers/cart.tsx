6b0dfafcb19dfd4db3ad86b5c9d324e10f5fd90d
matutaj <matutajorge2002@gmail.com>
2023-12-27 14:53:32 +0100

    feat: ensure product quantity can be product

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { ProductWithTotalPrice } from "@/helpers/product";
-import { Product } from "@prisma/client";
 import { ReactNode, createContext, useState } from "react";
 
 export interface CartProduct extends ProductWithTotalPrice {
@@ -13,6 +12,7 @@ interface ICartContext {
   cartBasePrice: number;
   cartTotalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -21,6 +21,7 @@ export const CartContext = createContext<ICartContext>({
   cartBasePrice: 0,
   cartTotalPrice: 0,
   addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -48,11 +49,27 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
 
     setProducts((prev) => [...prev, product]);
   };
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            };
+          }
+          return cartProduct;
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0),
+    );
+  };
   return (
     <CartContext.Provider
       value={{
         products,
         addProductToCart,
+        decreaseProductQuantity,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
         cartTotalPrice: 0,
