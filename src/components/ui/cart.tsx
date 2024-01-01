528381755f57e469fb1b9593606164d658d487bc
matutaj <matutajorge2002@gmail.com>
2024-01-01 09:57:44 +0100

    feat: add

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -7,6 +7,8 @@ import { ComputeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
+import Link from "next/link";
+import { SheetClose } from "./sheet";
 
 const Cart = () => {
   const { products, total, subTotal, totalDiscount } = useContext(CartContext);
@@ -38,31 +40,38 @@ const Cart = () => {
           </div>
         </ScrollArea>
       </div>
+      {products.length > 0 && (
+        <div className="flex flex-col gap-3">
+          <Separator />
 
-      <div className="flex flex-col gap-3">
-        <Separator />
-
-        <div className="flex items-center justify-between text-xs">
-          <p>SubTotal</p>
-          <p>Kz{subTotal.toFixed(2)}</p>
+          <div className="flex items-center justify-between text-xs">
+            <p>SubTotal</p>
+            <p>Kz{subTotal.toFixed(2)}</p>
+          </div>
+          <Separator />
+          <div className="flex items-center justify-between text-xs">
+            <p>Entrega</p>
+            <p>Grátis</p>
+          </div>
+          <Separator />
+          <div className="flex items-center justify-between text-xs">
+            <p>Descontos</p>
+            <p>- Kz{totalDiscount.toFixed(2)}</p>
+          </div>
+          <Separator />
+          <div className="flex items-center justify-between text-sm font-bold">
+            <p>Total</p>
+            <p>Kz{total.toFixed(2)}</p>
+          </div>
+          <SheetClose asChild>
+            <Link href="/">
+              <Button className="mt-7 font-bold uppercase">
+                Finalizar Compra
+              </Button>
+            </Link>
+          </SheetClose>
         </div>
-        <Separator />
-        <div className="flex items-center justify-between text-xs">
-          <p>Entrega</p>
-          <p>Grátis</p>
-        </div>
-        <Separator />
-        <div className="flex items-center justify-between text-xs">
-          <p>Descontos</p>
-          <p>- Kz{totalDiscount.toFixed(2)}</p>
-        </div>
-        <Separator />
-        <div className="flex items-center justify-between text-sm font-bold">
-          <p>Total</p>
-          <p>Kz{total.toFixed(2)}</p>
-        </div>
-        <Button className="mt-7 font-bold uppercase"> Finalizar Compra</Button>
-      </div>
+      )}
     </div>
   );
 };
