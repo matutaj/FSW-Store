06cbfe516159f5fa34505a5bbb540641cb099a79
matutaj <matutajorge2002@gmail.com>
2024-01-06 21:12:12 +0100

    add

--- next.config.js
+++ next.config.js
@@ -1,22 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
+  reactStrictMode: true,
+
   images: {
     domains: ["fsw-store.s3.sa-easttre-1.amazonaws.com"],
-    // // formats: ["image/avif", "image/webp"],
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: "fsw-store.s3.sa-easttre-1.amazonaws.com",
-    //     port: "3000",
-    //     pathname: "**",
-    //   },
-    // ],
     unoptimized: true,
   },
-  // experimental: {
-  //   serverActions: true,
-  // },
 };
 
 module.exports = nextConfig;
